7c4eddd9496588f2dd5ef63baf35bdf444ac1f16
Hazel <43965865+HazelTea@users.noreply.github.com>
2025-10-31 10:03:58 +0100

    Zadania Update

---
+++ Szkolne-Duperele-main/PHP/Zad1/Zad1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    
+    <?php
+    echo "<p> Tu funkcja echo <p>";
+    echo '<p> test' . 'funkcji <p>';
+    echo 'test z br <br>';
+    print('zgłasza się funkcja print');
+    ?>
+
+</body>
+</html>
