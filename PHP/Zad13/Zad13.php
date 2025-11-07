4e87b4b62e42535aff010786cd6df34458a0b5de
Dominik Glejzer <ct22glejzerd@zsi.kielce.pl>
2025-11-07 10:53:58 +0100

    Zad 13

---
+++ PHP/Zad13/Zad13.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pole trapezu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+        $a = 16;
+        $h = 64;
+        $result = $a * $h;
+    ?>
+    <p class="tekst"> <?= "Pole równoległoboku o podstawie długości $a i wysokości $h jest równe $result j²." ?></p>
+</body>
+</html>
